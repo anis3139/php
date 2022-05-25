4e028a0ab6a170da0738cf1daa00e1b1e5abe447
weDevs <38912435+anis3139@users.noreply.github.com>
2022-05-25 22:18:24 +0600

    class 3

--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
-$x = 9;
-//some chenges add-log-component
-// merge test
-echo sqrt($x);
-?>
+
+/**
+ * count word/character
+ * word/character position
+ * chracter slice
+ * word/ charecter replace
+ * reverce
+ */
+
+// $x = 'hello world   '; 
+// $arr=ltrim($x);
+// echo ucfirst($arr);
+
+// $number = 9;
+// $str = "Beijing";
+// printf("There are %u million bicycles in %s.",$number,$str);
+
+// $str = "Hello World";
+// echo str_pad($str,20," ....");
+
+
+// echo ucwords("I love php, I love php too!");
+
+
+// $arr=['mangoo', 'banana', 'orange'];
+// echo implode('-', $arr);
+
+$str='hello world';           
+echo print_r(explode(' ', $str));
+
+$arr = array('Hello','World!','Beautiful','Day!');
+echo join("- ",$arr);
