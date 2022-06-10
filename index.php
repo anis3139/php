dffea37b7f9a61697f3a4a955cbb177edb28e868
weDevs <38912435+anis3139@users.noreply.github.com>
2022-06-10 09:40:48 +0600

    class 7

--- index.php
+++ index.php
@@ -1,119 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
+    <!-- GET, POST, PUT/PATCH, DELETE -->
 
-// $arr=[
-//     ['id'=>1, 'name'=>'anis', 'age'=>24, ['address'=>[
-//         'vill'=>'pilgiri', 'p.o'=>'adda', 'p.s'=>'barura', 'dist'=>'comilla'
-//     ]]],
-//     ['id'=>2, 'name'=>'lima', 'age'=>22],
-//     ['id'=>3,'name'=>'rakhia', 'age'=>20]
-// ];
+    <form action="./data.php" method="POST" enctype="multipart/form-data">
+      <label for="fName">First Name</label>
+      <input type="text" id="fName" name="fName"> <br><br>
+      <label for="lName">Last Name</label>
+      <input type="text" id="lName" name="lName"> <br><br>
+      <input type="file" name="image[]" id="image"><br><br>
+      <input type="file" name="image[]" id="image"><br><br>
+      <input type="submit" value="Submit">
+    </form>
 
-
-// foreach ($arr as $key => $value) {
-  
-//     foreach ($value as $key2 => $data) {
-//           echo '<pre>';
-//           print_r($data);
-//           echo '</pre>';
-//     }
-// }
-
-
-// $arr =[
-//         ['id'=>1, 'name'=>'anis', 'age'=>24],
-//         ['id'=>2, 'name'=>'lima', 'age'=>22],
-//         ['id'=>3,'name'=>'rakhia', 'age'=>20]
-// ];
-
-
-//  print_r(array_column($arr, 'name'));
-
-
-// $fname=array("Peter","Ben","Joe");
-// $age=array("35","37","43");
-
-// $c=array_combine($fname,$age);
-// print_r($c);
-
-// $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-// $a2=array("e"=>"red","b"=>"green","d"=>"blue");
-
-// $result=array_diff_key($a1,$a2);
-// print_r($result);
-
-
-
-// $a1=array(1,3,2,3,4);
-
-// $result= array_filter($a1,function($var){
-//     if ($var%2==0) {
-//         return true;
-//     }
-// });
-
-// echo '<pre>';
-// print_r($result);
-// echo '</pre>';
-
-// $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-// $a2=array("a"=>"red","f"=>"green","g"=>"blue");
-
-// $result=array_key_exists('a', $a1);
-// print_r($result);
-
-// function myfunction($v)
-// {
-//   return "mr/mrs. {$v}";
-// }
-
-// $a=array('anis','lima','shakil','rakhia');
-
-// echo '<pre>';
-// print_r(array_map("myfunction",$a));
-// echo '</pre>';
-
-// $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-// $a2=array("a"=>"red","f"=>"green","g"=>"blue");
-
-// $res= array_merge($a1, $a2);
-
-$a=array("red","green","blue");
-array_pop($a);
-array_push($a, 'anis', 'lima');
-
-
-// function myfunction($v1, $v2)
-// {
-//     return $v1 + $v2;
-// }
-
-// $a=array(1,2,3,4);
-
-// print_r(array_reduce($a, "myfunction"));
-
-
-// $a=array("a"=>"red","b"=>"green","c"=>"blue");
-
-// echo array_shift($a);
-
-
-// $a=array("red","green","blue","yellow","brown");
-
-// echo '<pre>';
-// print_r(array_slice($a, 2, 2)); 
-// echo '</pre>';
-
-// $a1=array("a"=>"red","b"=>"green","c"=>"blue","d"=>"yellow");
-// $a2=array("a"=>"purple","b"=>"orange");
-// array_splice($a1,0,1, ['anis', 'shakil']);
-// echo '<pre>';
-// print_r($a1);
-// echo '</pre>';
-
-// $range= range(1,100);
-
-// echo '<pre>';
-// print_r($range);
-// echo '</pre>';
- 
+</body>
+</html>
