5ab481e8ef9cbad8a73638fbc51bf1a17f7efd62
weDevs <38912435+anis3139@users.noreply.github.com>
2022-06-23 09:43:15 +0600

    Class 8 (Solve edit error )

--- UpdateData.php
+++ UpdateData.php
@@ -1,18 +1,22 @@
 <?php
-$mysql= mysqli_connect('localhost', 'anis', 'password', 'school') or die('Connection Error');
-  $id=$_POST['id'];
-  $name=$_POST['name'];
-  $class=$_POST['class'];
-  $district=$_POST['district']; 
+    $mysql= mysqli_connect('localhost', 'anis', 'password', 'school') or die('Connection Error');
+    $id=$_POST['id'];
+    $name=$_POST['name'];
+    $class=$_POST['class'];
+    $district=$_POST['district'];
 
-  $sql="UPDATE students SET (name= '$name' class= '$class', district='$district') WHERE id='$id'" ;
+  try {
+      $sql="UPDATE students SET name= '{$name}', class= '{$class}', district='{$district}' WHERE id={$id}" ;
 
-  $result=mysqli_query($mysql, $sql) or die("No Result Found");
+      $result=mysqli_query($mysql, $sql);
+  } catch (\Throwable $th) {
+      throw $th->getMessage();
+  }
   
-  if( $result){
+  if ($result) {
       header("Location: ./index.php");
-  }else{
-    header("Location: ./edit.php");
+  } else {
+      header("Location: ./edit.php");
   }
 
 mysqli_close($mysql);
