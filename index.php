73ebf0b49ea19e3df55143428aa7577cea61e074
Md. Anichur Rahaman <38912435+anis3139@users.noreply.github.com>
2022-07-23 23:19:27 +0600

    class 13

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-    require_once('./header.php');
-  ?>
 
-<div class="row m-5">
-  <?php  include('./errorMessage.php') ;?>
-  <h1>Welcome to Dashborad </h1>
-</div>
 
-<?php
-  require_once('./footer.php')
-  ?>
-<!-- end file -->
+require_once('./DB.php');
+
+$db=new DB();
+
+$res=$db->get('users', '*');
+var_dump($res);
+
+
+ 
